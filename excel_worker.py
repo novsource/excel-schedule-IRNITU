cc33229cc5998b4d5ac0247f1c1f042bfa49c5a4
Evstafev <evstafev.nik@yandex.ru>
2021-07-04 14:32:14 +0800

    Add method of print data from excel

--- excel_worker.py
+++ excel_worker.py
@@ -1,8 +1,20 @@
 from xls2xlsx import XLS2XLSX
 from openpyxl import load_workbook
 
+book = load_workbook('data/3.xlsx')
 
 def convert_to_xlsx(path_to_file):
     x2x = XLS2XLSX(path_to_file)
     x2x.to_xlsx(path_to_file + 'x')
     print('Файл успешно конвертирован!')
+
+
+def print_data_from_excel():
+    for sheetname in book.sheetnames:
+        worksheet = book[sheetname]
+        for row in worksheet.iter_rows():
+                for cell in row:
+                   if cell.value != None:
+                       print(cell.value)
+                       print('\t', end='')
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import excel_worker
 
 
 def main():
-    excel_worker.convert_to_xlsx('data/3.xls')
+    excel_worker.print_data_from_excel()
 
 
 if __name__ == '__main__':
